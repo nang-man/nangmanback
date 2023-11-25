c88b0126a302b47ba496de10de2917a7684e4aa5
paikpaik <woduf9311@gmail.com>
2023-11-25 15:37:27 +0900

    [ADD]: UserSchema phone

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,6 +19,11 @@ const userSchema = new mongoose.Schema(
       maxlength: 8,
       required: true,
     },
+    phone: {
+      type: String,
+      unique: true,
+      required: true,
+    },
     password: {
       type: String,
       trim: true,
@@ -26,8 +31,8 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     adminState: {
-      type: Number,
-      default: 0,
+      type: Boolean,
+      default: false,
     },
     followings: [
       {
