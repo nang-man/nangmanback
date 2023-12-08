fb14437d2a2d3136945eca05f00752879371decb
ImgyeongLee <mingbab33@gmail.com>
2023-12-07 23:17:09 -0800

    [FIX] updated model attributes

--- src/models/room.model.js
+++ src/models/room.model.js
@@ -2,40 +2,35 @@ import mongoose from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 
 const roomSchema = new mongoose.Schema(
-  {
-    roomId: {
-      type: String,
-      default: uuidv4,
-      unique: true,
-      required: true,
-    },
-    roomName: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    maxNum: {
-        type: Number,
-        required: true,
-    },
-    isGuest: {
-        type: Boolean,
-        default: true,
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-    },
-    participants: {
-        type: [ mongoose.Schema.Types.ObjectId ],
-        required: true
-    },
-    chats: {
-        type: [ mongoose.Schema.Types.ObjectId ],
-        default: [],
-    }
-  },
-  { timestamps: true }
+   {
+      roomId: {
+         type: String,
+         default: uuidv4,
+         unique: true,
+         required: true,
+      },
+      roomName: {
+         type: String,
+         required: true,
+      },
+      maxNum: {
+         type: Number,
+         required: true,
+      },
+      owner: {
+         type: mongoose.Schema.Types.ObjectId,
+         required: true,
+      },
+      participants: {
+         type: [mongoose.Schema.Types.ObjectId],
+         required: true,
+      },
+      chats: {
+         type: [mongoose.Schema.Types.ObjectId],
+         default: [],
+      },
+   },
+   { timestamps: true }
 );
 
 const Room = mongoose.model("Room", roomSchema);
